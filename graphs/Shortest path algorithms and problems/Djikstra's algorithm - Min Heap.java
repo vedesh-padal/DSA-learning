49a252093f6c8360a42dfcbbf1d1a94f2cc64b8e
vedesh-padal <vedeshpadal@gmail.com>
2024-09-06 17:04:58 +0530

    Corrected Djikstras algorithm

--- graphs/Shortest path algorithms and problems/Djikstra's algorithm - Min Heap.java
+++ graphs/Shortest path algorithms and problems/Djikstra's algorithm - Min Heap.java
@@ -3,7 +3,6 @@
 // second integers corresponds to the weight of that  edge . You are given the source vertex S and 
 // You to Find the shortest distance of all the vertex's from the source vertex S. 
 // You have to return a list of integers denoting shortest distance between each node and Source vertex S.
- 
 
 // Note: The Graph doesn't contain any negative weight cycle.
 
@@ -33,8 +32,7 @@ class Node {
   }
 }
 
-class Solution
-{
+class Solution {
   // Function to find the shortest distance of all the vertices
   // from the source vertex S.
   static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
@@ -42,57 +40,63 @@ class Solution
 
     //// ===> USE THIS:
     // // Approach - 1: Using Priority Queue (specifically min-heap)
-    // PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
-    // int[] dist = new int[V];
-    // for (int i = 0; i < V; i++)
-    //   dist[i] = (int) (1e9);
+    PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
+    int[] dist = new int[V];
+    for (int i = 0; i < V; i++)
+      dist[i] = (int) (1e9);
 
-    // dist[S] = 0;
-    // pq.add(new Pair(0, S));
+    dist[S] = 0;
+    pq.add(new Pair(0, S));
 
-    // while (pq.size() != 0) {
-    //   // int dis = pq.peek().distance;
-    //   int node = pq.peek().node;
-    //   pq.remove();
+    while (pq.size() != 0) {
+      // int dis = pq.peek().distance;
+      int node = pq.peek().node;
+      pq.remove();
 
-    //   for (int i = 0; i < adj.get(node).size(); i++) {
-    //     int edgeWeight = adj.get(node).get(i).get(1);
-    //     int adjNode = adj.get(node).get(i).get(0);
+      for (int i = 0; i < adj.get(node).size(); i++) {
+        int edgeWeight = adj.get(node).get(i).get(1);
+        int adjNode = adj.get(node).get(i).get(0);
 
-    //     // now, compare which distance is least
-    //     if (dist[node] + edgeWeight < dist[adjNode]) {
-    //       dist[adjNode] = dist[node] + edgeWeight;
-    //       pq.add(new Pair(dist[adjNode], adjNode));
-    //     }
-    //   }
-    // }
-    // return dist;
-        
-        
-    // using TreeSet - but not working properly
-    // TreeSet<Pair> set = new TreeSet<>((x, y) -> (x.distance - y.distance));
-    // int[] dist = new int[V];
-    // for (int i=0; i<V; i++) 
-    //     dist[i] = (int)(1e9);
-        
-    // dist[S] = 0;
-    // set.add(new Pair(0, S));
-    
-    // while (set.size() != 0)  {
-    //     Pair pair = set.pollFirst();
-        
-    //     for (ArrayList<Integer> adjNodes: adj.get(pair.node))    {
-    //         int adjNode = adjNodes.get(0);
-    //         int edgeWeight = adjNodes.get(1);
-            
-    //         // now, compare which distance is least
-    //         if (dist[pair.node] + edgeWeight < dist[adjNode])    {
-    //             dist[adjNode] = dist[pair.node] + edgeWeight;
-    //             set.add(new Pair(dist[adjNode], adjNode));
-    //         }
-    //     }
-    // }
-    // return dist;
+        // now, compare which distance is least
+        if (dist[node] + edgeWeight < dist[adjNode]) {
+          dist[adjNode] = dist[node] + edgeWeight;
+          pq.add(new Pair(dist[adjNode], adjNode));
+        }
+      }
+    }
+    return dist;
+  }
+
+  // using TreeSet - but not working properly
+  static int[] dijkstra2(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
+
+    TreeSet<Pair> set = new TreeSet<>((x, y) -> (x.distance - y.distance));
+    int[] dist = new int[V];
+    for (int i = 0; i < V; i++)
+      dist[i] = (int) (1e9);
+
+    dist[S] = 0;
+    set.add(new Pair(0, S));
+
+    while (set.size() != 0) {
+      Pair pair = set.pollFirst();
+
+      for (ArrayList<Integer> adjNodes : adj.get(pair.node)) {
+        int adjNode = adjNodes.get(0);
+        int edgeWeight = adjNodes.get(1);
+
+        // now, compare which distance is least
+        if (dist[pair.node] + edgeWeight < dist[adjNode]) {
+          dist[adjNode] = dist[pair.node] + edgeWeight;
+          set.add(new Pair(dist[adjNode], adjNode));
+        }
+      }
+    }
+    return dist;
+  }
+
+  // working method using TreeSet
+  static int[] dijkstra3(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
 
     TreeSet<Node> set = new TreeSet<>((node1, node2) -> {
       if (node1.value != node2.value && node1.dist == node2.dist) {
