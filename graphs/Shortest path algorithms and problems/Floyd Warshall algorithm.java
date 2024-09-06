2652e0f69471784659d1ce66582b2c27d27ebb5d
vedesh-padal <vedeshpadal@gmail.com>
2024-09-06 16:27:04 +0530

    Floyd Warshall algorithm

---
+++ graphs/Shortest path algorithms and problems/Floyd Warshall algorithm.java
@@ -0,0 +1,38 @@
+/*
+  The problem is to find the shortest distances between every pair of vertices 
+  in a given edge-weighted directed graph. The graph is represented as an adjacency 
+  matrix of size n*n. Matrix[i][j] denotes the weight of the edge from i to j. 
+  If Matrix[i][j]=-1, it means there is no edge from i to j.
+  Note : Modify the distances for every pair in-place.
+*/
+
+class Solution {
+  public void shortest_distance(int[][] matrix) {
+    // Code here
+    int n = matrix.length;
+    int m = matrix[0].length;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (matrix[i][j] == -1)
+          matrix[i][j] = (int) (1e5);
+      }
+    }
+
+    for (int k = 0; k < n; k++) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if (matrix[i][k] + matrix[k][j] < matrix[i][j])
+            matrix[i][j] = matrix[i][k] + matrix[k][j];
+        }
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (matrix[i][j] == (int) (1e5))
+          matrix[i][j] = -1;
+      }
+    }
+  }
+}
