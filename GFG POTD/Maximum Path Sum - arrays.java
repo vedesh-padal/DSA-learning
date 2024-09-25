7c9795a56562fb9c86edc0eb19440cd7fe93e42d
vedesh-padal <vedeshpadal@gmail.com>
2024-09-25 12:06:38 +0530

    GFG POTD 25-09-2024

---
+++ GFG POTD/Maximum Path Sum - arrays.java
@@ -0,0 +1,70 @@
+/*
+ * Given two sorted arrays of distinct integers arr1 and arr2. Each array may 
+ * have some elements in common with the other array. Find the maximum sum of a 
+ * path from the beginning of any array to the end of any array. You can switch 
+ * from one array to another array only at the common elements.
+
+  Note:  When we switch from one array to other,  we need to consider the common
+  element only once in the result.
+
+  Examples : 
+  Input: arr1 = [2, 3, 7, 10, 12] , arr2 = [1, 5, 7, 8]
+  Output: 35
+  Explanation: The path will be 1+5+7+10+12 = 35, where 1 and 5 come from arr2 and then 7 is common so we switch to arr1 and add 10 and 12.
+
+  Input: arr1 = [1, 2, 3] , arr2[] = [3, 4, 5]
+  Output: 15
+  Explanation: The path will be 1+2+3+4+5=15.
+
+  Expected Time Complexity: O(m + n)
+  Expected Auxiliary Space: O(1)
+
+  Constraints:
+  1 <= arr1.size(), arr2.size() <= 104
+  1 <= arr1[i], arr2[i] <= 105
+ */
+import java.util.List;
+
+class Solution {
+  public int maxPathSum(List<Integer> arr1, List<Integer> arr2) {
+    // code here
+    // MERGE SORT LOGIC
+    int i = 0, j = 0;
+    int sum1 = 0, sum2 = 0;
+    int n = arr1.size(), m = arr2.size();
+
+    int result = 0;
+
+    while (i < n && j < m) {
+      if (arr1.get(i) < arr2.get(j)) {
+        sum1 += arr1.get(i);
+        i++;
+      } else if (arr2.get(j) < arr1.get(i)) {
+        sum2 += arr2.get(j);
+        j++;
+      } else {
+        // for common element
+        // consider it once and take the max. of sum1 and sum2
+        result += Math.max(sum1, sum2) + arr1.get(i);
+
+        sum1 = 0;
+        sum2 = 0;
+        i++;
+        j++;
+      }
+    }
+
+    while (i < n) {
+      sum1 += arr1.get(i);
+      i++;
+    }
+    while (j < m) {
+      sum2 += arr2.get(j);
+      j++;
+    }
+
+    result += Math.max(sum1, sum2);
+
+    return result;
+  }
+}
