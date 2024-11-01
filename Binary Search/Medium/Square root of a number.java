9d34a43cdb85f52ac4593f1cc9b072e21c0def69
vedesh-padal <vedeshpadal@gmail.com>
2024-11-01 12:31:04 +0530

    code quality improved for square root of a number question

--- Binary Search/Medium/Square root of a number.java
+++ Binary Search/Medium/Square root of a number.java
@@ -14,4 +14,28 @@ class Solution {
     }
     return ans;
   }
+
+  public int mySqrt(int x) {
+    // binary search
+    if (x == 0 || x == 1)
+      return x;
+
+    int start = 1;
+    int end = x;
+    int mid;
+    while (start <= end) {
+      mid = start + (end - start) / 2;
+
+      if ((long) mid * mid > (long) x)
+        end = mid - 1;
+
+      else if (mid * mid == x)
+        return mid;
+        
+      else
+        start = mid + 1;
+    }
+
+    return Math.round(end);
+  }
 }
