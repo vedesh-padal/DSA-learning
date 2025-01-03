1c49aafc219182b40af7a6bca80137b2e34e41dc
vedesh-padal <vedeshpadal@gmail.com>
2025-01-03 18:05:06 +0530

    LC Daily 01-03 Jan 2025

---
+++ Leetcode-daily/2025-01/02 - 2559 - Count vowel strings in ranges.java
@@ -0,0 +1,66 @@
+// MEDIUM - EASY
+// arrays, prefix-sum, string
+
+/*
+    You are given a 0-indexed array of strings words and a 2D array of integers queries.
+
+    Each query queries[i] = [li, ri] asks us to find the number of strings
+    present in the range li to ri (both inclusive) of words that start and end with a vowel.
+
+    Return an array ans of size queries.length, where ans[i] is the answer to the ith query.
+
+    Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
+
+    Example 1:
+    Input: words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
+    Output: [2,3,0]
+    Explanation: The strings starting and ending with a vowel are "aba", "ece",
+    "aa" and "e".
+    The answer to the query [0,2] is 2 (strings "aba" and "ece").
+    to query [1,4] is 3 (strings "ece", "aa", "e").
+    to query [1,1] is 0.
+    We return [2,3,0].
+
+    Example 2:
+    Input: words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
+    Output: [3,2,1]
+    Explanation: Every string satisfies the conditions, so we return [3,2,1].
+
+    Constraints:
+    1 <= words.length <= 10^5
+    1 <= words[i].length <= 40
+    words[i] consists only of lowercase English letters.
+    sum(words[i].length) <= 3 * 10^5
+    1 <= queries.length <= 10^5
+    0 <= li <= ri < words.length
+*/
+
+class Solution {
+    private boolean isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int n = words.length, m = queries.length;
+        int[] preSum = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1))) {
+                preSum[i] = 1;
+            }
+        }
+
+        // now accumulate them => prefix sum
+        for (int i = 1; i < n; i++) {
+            preSum[i] += preSum[i-1];
+        }
+        
+        int[] res = new int[m];
+        for (int i = 0; i < m; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            res[i] = preSum[end] - ((start - 1 >= 0) ? preSum[start - 1] : 0);
+        }
+        return res;
+    }
+}
